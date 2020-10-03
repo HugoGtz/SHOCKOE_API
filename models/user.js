c4c0b17a197cfd256791b85cb72e4933710a8c0c
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-03 11:24:07 -0500

    feat: add unique user_name, add associate with task, and add hook to hash password before create record

--- models/user.js
+++ models/user.js
@@ -1,12 +1,26 @@
 'use strict';
+import hash_password from 'helpers/hash_password'
+
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         name: DataTypes.STRING,
-        user_name: DataTypes.STRING,
-        passord: DataTypes.STRING
+        user_name: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            unique: true
+        },
+        password: DataTypes.STRING
     }, {});
+    
     User.associate = function(models) {
-        User.hasMany(models.Task, {foreignKey: 'user_id'})
+        User.hasMany(models.Task, {
+            foreignKey: 'user_id'
+        })
     };
+
+    User.addHook('afterValidate', 'hashPassword', (user, options) => {
+        user.password = hash_password(user.password)
+    });
+
     return User;
 }
