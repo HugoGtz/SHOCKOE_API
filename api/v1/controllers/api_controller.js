1d473c0afec86fe6201dd1e31539c54fbee3c297
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-03 11:12:27 -0500

    feat: add mixins controller

--- api/v1/controllers/api_controller.js
+++ api/v1/controllers/api_controller.js
@@ -1,3 +1,16 @@
 export default class ApiController {
-    constructor() {}
+    constructor(){
+    }
+
+    params_permit(req, params) {
+        return req.parameters.permit(params).value()
+    }
+
+    res_success(res, data, statusCode=200) {
+        return res.status(statusCode).json(data)
+    }
+
+    res_fail(res, err, statusCode=500) {
+        return res.status(statusCode).json({err: err})
+    }
 }
