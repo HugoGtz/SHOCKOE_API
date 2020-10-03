b6470a98515e415ed2aa84c9b511ee28d2bc9cd8
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-02 19:31:14 -0500

    feat: add sequelize config json

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports =  {
+  development: {
+    username: 'root',
+    password: '123456',
+    database: 'shockoe_api_development',
+    host: 'localhost',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    dialect: 'mysql'
+  }
+}
