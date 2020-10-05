915c70740def689dde659e5626f54d7b3076b8ec
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 13:46:07 -0500

    feat: add crud user routes

--- api/v1/router/user_router.js
+++ api/v1/router/user_router.js
@@ -4,6 +4,10 @@ import UserController from 'api/v1/controllers/user_controller'
 let router = Router()
 
 router.post('/', UserController.create.bind(UserController))
+router.get('/:id', UserController.show.bind(UserController))
+router.get('/', UserController.index.bind(UserController))
+router.put('/:id', UserController.update.bind(UserController))
+router.delete('/:id', UserController.destroy.bind(UserController))
 
 
 export default router
