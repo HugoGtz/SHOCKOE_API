07fef5fa84cf9ac12f239d73a6699d46021615e2
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 13:45:01 -0500

    feat: add user update function

--- api/v1/controllers/user_controller.js
+++ api/v1/controllers/user_controller.js
@@ -35,6 +35,17 @@ class UserController extends ApiController {
         })
     }
 
+    @functionHandler()
+    update(req, res) {
+        User.findByPk(this.id).then((user) => {
+            return user.update(this.userParams)
+        }).then((user) => {
+            return this.res_success(user)
+        }).catch((err) => {
+            return this.res_fail(String(err))
+        })
+    }
+
     get userParams() {
         let params = ['name', 'username', 'password']
         return this.params_permit(params)
