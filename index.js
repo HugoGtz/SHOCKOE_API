c2d00ad15eed2aede3827b21e7f862d534c09c1a
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 13:49:23 -0500

    fix: import server config, and just run server

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-import express from 'express'
-import cors from 'cors'
-import v1 from 'api/v1'
-import morgan from 'morgan'
-
-let app = express()
-
-app.use(morgan('combined'))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/v1', v1)
-
-app.get('*', function(req, res) {
-    res.status(404).json({error: "Resource not found"})
-});
+import app from 'server'
 
 app.listen(3000, () => {
     console.log('Api listening on port 3000!')
