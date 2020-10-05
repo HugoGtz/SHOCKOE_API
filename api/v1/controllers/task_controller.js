347bb419ccb7d170a33e83bc2c120dbc3108ce62
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 14:32:13 -0500

    fix: change user create function with functionHandler

--- api/v1/controllers/task_controller.js
+++ api/v1/controllers/task_controller.js
@@ -1,8 +1,25 @@
 import ApiController from './api_controller'
+import functionHandler from 'helpers/function_handler'
+import { Task } from 'models'
 
-class TaskController extends ApiController{
+
+class TaskController extends ApiController {
+    constructor() {
+        super()
+    }
+
+    @functionHandler()
     create(req, res) {
-        return res.json({status: true})
+        Task.create(this.taskParams()).then((user) => {
+            return this.resSuccess(user)
+        }).catch((err) => {
+            return this.resFail(String(err))
+        })
+    }
+
+    taskParams(addParams=[]) {
+        let params = ['name', 'due_date', 'description']
+        return this.paramsPermit(params.concat(addParams))
     }
 }
 
