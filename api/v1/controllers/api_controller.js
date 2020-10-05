26360137e7ce822584ef6b252b6e02fb7a4b90a4
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 13:37:15 -0500

    fix: private function was create to reuse with response

--- api/v1/controllers/api_controller.js
+++ api/v1/controllers/api_controller.js
@@ -1,16 +1,25 @@
 export default class ApiController {
-    constructor(){
+    constructor() {
+        this.req = null
+        this.res = null
+        this.params = null
     }
 
-    params_permit(req, params) {
-        return req.parameters.permit(params).value()
+    params_permit(params) {
+        return this.req.parameters.permit(params).value()
     }
 
-    res_success(res, data, statusCode=200) {
-        return res.status(statusCode).json(data)
+    res_success(data, statusCode = 200) {
+        return this.__res(data, statusCode)
     }
 
-    res_fail(res, err, statusCode=500) {
-        return res.status(statusCode).json({err: err})
+    res_fail(err, statusCode = 500) {
+        return this.__res({
+            err: err
+        }, statusCode)
+    }
+
+    __res(json, statusCode) {
+        this.res.status(statusCode).json(json)
     }
 }
