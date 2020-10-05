e1735a1c58e9510ae42987c64f80f4881bf911dc
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 13:43:38 -0500

    feat: add user index function

--- api/v1/controllers/user_controller.js
+++ api/v1/controllers/user_controller.js
@@ -9,6 +9,15 @@ class UserController extends ApiController {
     }
 
     @functionHandler()
+    index(req, res) {
+        User.findAll().then((users) => {
+            return this.res_success(users)
+        }).catch((err) => {
+            return this.res_fail(String(err))
+        })
+    }
+
+    @functionHandler()
     create(req, res) {
         User.create(this.userParams).then((user) => {
             return this.res_success(user)
