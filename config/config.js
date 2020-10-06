b37d542b06c13cafeb7a19d10aeb4d1dcb7dc563
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-06 18:57:29 -0500

    fix: adding env variables

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports =  {
   development: {
-    username: 'root',
-    password: '123456',
-    database: 'shockoe_api_development',
-    host: 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_development`,
+    host: process.env.DB_HOSTNAME,
     dialect: 'mysql'
   },
   test: {
-    username: 'root',
-    password: '123456',
-    database: 'shockoe_api_test',
-    host: 'localhost',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_test`,
+    host: process.env.DB_HOSTNAME,
     dialect: 'mysql',
     logging: false
   },
   production: {
     username: process.env.PROD_DB_USERNAME,
     password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
+    database: `${process.env.DB_NAME}_production`,
     host: process.env.PROD_DB_HOSTNAME,
     dialect: 'mysql'
   }
