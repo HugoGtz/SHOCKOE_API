13809f304273f2d666738c5b880dcb6a432340bf
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 19:05:20 -0500

    feat: adding user tasks route

--- api/v1/router/user_router.js
+++ api/v1/router/user_router.js
@@ -3,6 +3,7 @@ import UserController from 'api/v1/controllers/user_controller'
 
 let router = Router()
 
+router.get('/:id/tasks', UserController.tasks.bind(UserController))
 router.post('/', UserController.create.bind(UserController))
 router.get('/:id', UserController.show.bind(UserController))
 router.get('/', UserController.index.bind(UserController))
