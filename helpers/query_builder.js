46c5ad4be0909c2ec033807d007d749d424106a8
Hugo Gutierrez <arnoldohugo@gmail.com>
2020-10-05 19:06:13 -0500

    refactor: reuse iterator keys

--- helpers/query_builder.js
+++ helpers/query_builder.js
@@ -2,32 +2,37 @@ import _ from 'lodash'
 import { sequelize } from 'models'
 import { Op } from 'sequelize'
 
-
-export let like = (keysToCreate, params, querySearch = {}) => {
+export let filter = (fn, keysToCreate, params, querySearch) => {
     if (!_.isEmpty(params)) {
         let keys = Object.keys(params)
         let query = keysToCreate.forEach((key) => {
             if (keys.includes(key)) {
-                querySearch[key] = {
-                    [Op.like]: `%${params[key]}%`
-                }
+                querySearch = filterFunctions[fn](key, params, querySearch)
             }
         })
     }
     return querySearch
 }
 
-export let date = (keysToCreate, params, querySearch = {}) => {
-    if (!_.isEmpty(params)) {
-        let keys = Object.keys(params)
-        let query = keysToCreate.forEach((key) => {
-            if (keys.includes('due_date')) {
-                let with_date = sequelize.where(
-                    sequelize.fn('date', sequelize.col(key)),
-                    '=', params[key])
-                querySearch[key] = with_date
-            }
-        })
+let filterFunctions = {
+    like: (key, params, querySearch = {}) => {
+        querySearch[key] = {
+            [Op.like]: `%${params[key]}%`
+        }
+        return querySearch
+    },
+    date: (key, params, querySearch = {}) => {
+        let with_date = sequelize.where(
+            sequelize.fn('date', sequelize.col(key)),
+            '=', params[key]
+        )
+        querySearch[key] = with_date
+        return querySearch
+    },
+    is: (key, params, querySearch = {}) => {
+        querySearch[key] = {
+            [Op.is]: params[key]
+        }
+        return querySearch
     }
-    return querySearch
 }
